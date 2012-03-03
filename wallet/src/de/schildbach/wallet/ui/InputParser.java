afaaa89fe9c0ee05d0d1b123e2ad6ce761a64606
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version

--- wallet/src/de/schildbach/wallet/ui/InputParser.java
+++ wallet/src/de/schildbach/wallet/ui/InputParser.java
@@ -48,7 +48,7 @@ import com.google.protobuf.InvalidProtocolBufferException;
 import de.schildbach.wallet.Constants;
 import de.schildbach.wallet.PaymentIntent;
 import de.schildbach.wallet.util.Qr;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 /**
  * @author Andreas Schildbach
